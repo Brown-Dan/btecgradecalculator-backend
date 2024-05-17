cbd42779457f33afed677e91b4016e6496837b22
daniel.brown <daniel.brown@autotrader.co.uk>
2024-05-17 12:04:51 +0100

    added logger for grade requests received

--- src/main/java/uk/danbrown/btecgradecalculatorbackend/controller/GradeCalculationController.java
+++ src/main/java/uk/danbrown/btecgradecalculatorbackend/controller/GradeCalculationController.java
@@ -1,5 +1,7 @@
 package uk.danbrown.btecgradecalculatorbackend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +16,15 @@ public class GradeCalculationController {
 
     private final GradeCalculationService gradeCalculationService;
 
+    private static final Logger LOG = LoggerFactory.getLogger(GradeCalculationController.class);
+
     public GradeCalculationController(GradeCalculationService gradeCalculationService) {
         this.gradeCalculationService = gradeCalculationService;
     }
 
     @PostMapping()
     public ResponseEntity<?> calculateGrade(@RequestBody CourseGradeCalculationRequest courseGradeCalculationRequest) {
+        LOG.info("Received Grade Calculation Request with data - {}", courseGradeCalculationRequest);
         return ResponseEntity.ok(gradeCalculationService.calculateGrades(courseGradeCalculationRequest));
     }
 }
